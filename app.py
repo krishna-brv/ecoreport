57d4b7218b575f33367c34178969baed4137ce3f
krishna-brv <brahmadevararoopvenkatakrishna@gmail.com>
2026-01-31 03:58:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,10 @@ def get_db_connection():
         user=os.environ.get("MYSQLUSER"),
         password=os.environ.get("MYSQLPASSWORD"),
         database=os.environ.get("MYSQLDATABASE"),
-        port=os.environ.get("MYSQLPORT")
+        port=int(os.environ.get("MYSQLPORT", 3306)),
+        connection_timeout=5
     )
+
 
 
 # ---------- PUBLIC ----------
